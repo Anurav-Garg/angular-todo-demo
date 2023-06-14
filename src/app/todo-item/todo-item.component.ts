fd7abb94c75279e979d6ae86df12c471d683ae8e
Anurav-Garg <anurav.grg@gmail.com>
2023-06-14 15:24:04 +0530

    Make todo-item component

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css'],
+})
+export class TodoItemComponent {
+  @Input() checked: boolean = false;
+  @Input() id: number = 0;
+  task: string = '';
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  toggleCheck() {
+    this.checked = !this.checked;
+    this.checkedChange.emit(this.checked);
+  }
+}
