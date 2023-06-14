f0d0f623eb223440c1a425ac09261830d0c52ec9
Anurav-Garg <anurav.grg@gmail.com>
2023-06-14 22:59:06 +0530

    Make final main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todoDemo';
+
+  todoCards: number[] = [0];
+
+  addCard(): void {
+    this.todoCards.push(this.todoCards.length);
+  }
+
+  deleteCard(id: number) {
+    if (id < this.todoCards.length) {
+      this.todoCards.splice(id, 1);
+      for (let i = 0; i < this.todoCards.length; i++) {
+        this.todoCards[i] = i;
+      }
+    }
+  }
 }
