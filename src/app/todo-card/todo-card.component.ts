fb4f056ac6ff1fbd1833fc3c0adde311f08f3a59
Anurav-Garg <anurav.grg@gmail.com>
2023-06-14 20:21:54 +0530

    Update TodoCard to put checked elements at bottom

--- src/app/todo-card/todo-card.component.ts
+++ src/app/todo-card/todo-card.component.ts
@@ -11,6 +11,7 @@ export class TodoCardComponent {
     {
       id: 0,
       checked: false,
+      text: '',
     },
   ];
 
@@ -18,15 +19,30 @@ export class TodoCardComponent {
     this.todoItems.push({
       id: this.todoItems.length,
       checked: false,
+      text: '',
     });
   }
 
   deleteTask(id: number): void {
+    console.log(this.todoItems[id]);
+
     if (id < this.todoItems.length) {
       this.todoItems.splice(id, 1);
       this.todoItems.forEach((todoItem: TodoItem, index: number) => {
         todoItem.id = index;
       });
     }
+  }
+
+  findUncheckedTasks(): TodoItem[] {
+    return this.todoItems.filter((todoItem) => {
+      return !todoItem.checked;
+    });
+  }
+
+  findCheckedTasks(): TodoItem[] {
+    return this.todoItems.filter((todoItem) => {
+      return todoItem.checked;
+    });
   }
 }
